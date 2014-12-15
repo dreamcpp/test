afc2306aef76c9c3dc36e03d2b66306a28cb8aa0
dreamcpp <dream_cpp@163.com>
2014-12-15 11:57:33 +0800

    Update home_controllers.rb

--- app/controllers/home_controllers.rb
+++ app/controllers/home_controllers.rb
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-class HomeController < ApplicationController
-  def show
-=======
+
 class HomeControllers < ApplicationController
   def index
->>>>>>> cf1a841cf6dadcda537fcb0751324abbe3084210
   end
 end
